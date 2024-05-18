9486a9a119fc05ab59ff0c73639637057813fbdd
proyecto-telematica3 <proyecto.telematica3@gmail.com>
2024-05-18 11:34:56 -0500

    prueba sin auth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Home } from './components/Home';
 import { Login } from './components/Login';
 import { Productos } from './components/Productos';
 import { Routes,Route } from "react-router-dom"
-import { AuthProvider } from "./context/authContext";
+//import { AuthProvider } from "./context/authContext";
 import { Register } from "./components/Register";
 import { Crearp } from "./components/Crearp";
 import { Eliminarp } from "./components/Eliminarp";
@@ -12,7 +12,7 @@ function App()
 {
   return (
     <div className="bg-gradient-to-b from-slate-600 to-slate-100 h-screen text-white flex">
-      <AuthProvider>
+      <H1>
         <Routes>
           <Route path="/" element={<Login/>} />
           <Route path="/Login" element={<Login/>} />
@@ -22,7 +22,7 @@ function App()
           <Route path="/Crearp" element={<Crearp/>} />
           <Route path="/Eliminarp" element={<Eliminarp/>} />
         </Routes>      
-      </AuthProvider>
+      </H1>
       </div>
   );
 }
