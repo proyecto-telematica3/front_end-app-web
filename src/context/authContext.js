73d07cf8af24de30b2d9a7b2538a901bbd1e6550
proyecto-telematica3 <proyecto.telematica3@gmail.com>
2024-05-18 02:16:39 -0500

    test

---
+++ src/context/authContext.js
@@ -0,0 +1,23 @@
+/*import { createContext, useContext } from "react";
+import { createUserWithEmailAndPassword} from "firebase/auth";
+import { auth } from "firebase";
+
+export const authContext = createContext()
+
+export const useAuth = () => {
+    const context = useContext(authContext)
+    if(!context) throw new Error('No hay autenticador')
+    return context
+}
+
+export function AuthProvider({ children }) {
+    const signup = (email, password) => {
+      return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    return (
+        <authContext.Provider value={{ signup }}>
+            {children}
+        </authContext.Provider>
+    )
+}*/
